f73d0d2be8d177ce4beef0ab65b504c5a09efea3
Nikita <bng@1sm.ru>
2020-02-20 14:18:46 +0500

    1

--- js/slider.js
+++ js/slider.js
@@ -2,19 +2,30 @@ window.onload = function() {
 
     let hiddenElements = document.querySelectorAll('.brand-block__brands');
     let hiddenBrandBlock = document.querySelector('.brand-block');
-    if(window.innerWidth <= 768) {
 
-        var mySwiper = new Swiper('.swiper-container', {
-            pagination: {
-                el: '.swiper-pagination',
-            },
-        });
+
+    if(window.innerWidth <= 768) {
         hiddenBrandBlock.classList.remove('brand-block');
     }
 
+    if(window.innerWidth >= 768) {
+
+        if (typeof window.mySwiper !== "undefined"){
+            window.mySwiper.destroy(true, true);
+            document.querySelector('.swiper-pagination').style.display = 'none';
+        }
+
+        let hiddenElements = document.querySelectorAll('.brand-block__brands');
+
+        for (let i = 0; i < hiddenElements.length; i++) {
+            if (hiddenElements[i].classList.contains("swiper-slide")) {
+                hiddenElements[i].classList.remove('swiper-slide');
+
+            }
+        }
+    }
 
    function visibleMore () {
-
 
        for (let i = 0; i < hiddenElements.length; i++){
             if (hiddenElements[i].classList.contains("visible")) {
@@ -28,26 +39,11 @@ window.onload = function() {
                 iconReadMore.style.transform = "rotate(180deg)";
                 hiddenElements[i].classList.remove('unvisible');
             }
-
     }
 }
     let iconReadMore = document.querySelector('.icon-read-more');
     let buttonReadMore = document.querySelector('.read-more');
         buttonReadMore.addEventListener("click", visibleMore);
-
-    if(window.innerWidth >= 768) {
-    let hiddenElements = document.querySelectorAll('.brand-block__brands');
-    for (let i = 0; i < hiddenElements.length; i++) {
-        if (hiddenElements[i].classList.contains("swiper-slide")) {
-            hiddenElements[i].classList.remove('swiper-slide');
-        }
-      }
-    }
-
-
-
-
-
 
 
 
